b9752b3e6046f6b0f4401abcdb0038148dc9c7f6
nhering <nhering1@gmail.com>
2017-09-29 10:46:11 -0700

    Building models and updates to frameworks

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
